f9a1b44e7891b457c480131ccf0129840d6937e2
LTTTTTE <10100aaaaa@gmail.com>
2018-05-26 23:15:04 +0900

    Box2d 물리엔진 2

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -15,12 +15,35 @@ bool HelloWorld::init()
 	{
 		return false;
 	}
-	
-	texture = Director::getInstance()->getTextureCache()->addImage("Globe_48px.png");
-	
+
+	if (this->createBox2dWorld(debug)) 
+		this->schedule(schedule_selector(HelloWorld::tick));
+
+	return true;
+}
+
+bool HelloWorld::createBox2dWorld(bool debug)
+{
 	world = new b2World(b2Vec2(0, -30));
 	world->SetAllowSleeping(true);
 	world->SetContinuousPhysics(true);
+
+	if (debug) {
+
+		debugDraw = new GLESDebugDraw(PTM_RATIO);
+		world->SetDebugDraw(debugDraw);
+
+		uint32 flags = 0;
+
+		flags += b2Draw::e_shapeBit;
+		//flags += b2Draw::e_jointBit;
+		//flags += b2Draw::e_aabbBit;
+		//flags += b2Draw::e_pairBit;
+		//flags += b2Draw::e_centerOfMassBit;
+		debugDraw->SetFlags(flags);
+
+	}
+
 
 	// 윌드안에 바디가 있고 바디는 픽스쳐를 가진다.??
 	// 바디도 def(속성)를 가지고??  픽스쳐도 def(속성)를 가진다??
@@ -34,9 +57,9 @@ bool HelloWorld::init()
 	b2FixtureDef boxShape_Def;  // 픽스쳐속성 설정 
 	boxShape_Def.shape = &worldEdge;  //바디->픽스처넣기메소드(픽스쳐설정가진객체)  // 픽스쳐설정가진객체.shape 로 모양만들기
 
-	//픽스쳐는 바디의 뼈대같은거니깐 선(EdgeShape) 으로 만든다?
+									  //픽스쳐는 바디의 뼈대같은거니깐 선(EdgeShape) 으로 만든다?
 
-	//아래는 [바디를구성할 픽스쳐]를 구성할 선을 설정후 바디의 픽스쳐로만드는코드
+									  //아래는 [바디를구성할 픽스쳐]를 구성할 선을 설정후 바디의 픽스쳐로만드는코드
 
 	worldEdge.Set(b2Vec2(0, 0), b2Vec2(winsize.width / PTM_RATIO, 0));  //(0,0)에서 (가로길이,0) 좌표를 가지는 b2EdgeShape클래스 객체(선??)
 	worldBody->CreateFixture(&boxShape_Def);
@@ -44,45 +67,78 @@ bool HelloWorld::init()
 	worldEdge.Set(b2Vec2(0, winsize.height / PTM_RATIO), b2Vec2(winsize.width / PTM_RATIO, winsize.height / PTM_RATIO));
 	worldBody->CreateFixture(&boxShape_Def);
 
-	worldEdge.Set(b2Vec2(0, 0), b2Vec2(0, winsize.height / PTM_RATIO));  
+	worldEdge.Set(b2Vec2(0, 0), b2Vec2(0, winsize.height / PTM_RATIO));
 	worldBody->CreateFixture(&boxShape_Def);
 
 	worldEdge.Set(b2Vec2(winsize.width / PTM_RATIO, 0), b2Vec2(winsize.width / PTM_RATIO, winsize.height / PTM_RATIO));
 	worldBody->CreateFixture(&boxShape_Def);
 
-////////
-	auto spr_droid = Sprite::create("Android_48px.png");
-	spr_droid->setPosition(50, 50);
-	this->addChild(spr_droid, 0, "spr_droid");
+	return true;
+}
+
+void HelloWorld::draw(Renderer * renderer, const Mat4 & transform, uint32_t flags)
+{
+	Scene::draw(renderer, transform, flags);
+	custom_cmd.init(_globalZOrder, transform, flags);
+	custom_cmd.func = CC_CALLBACK_0(HelloWorld::onDraw, this, transform, flags);
+	renderer->addCommand(&custom_cmd);
+	
+}
+
+void HelloWorld::onDraw(const Mat4 & transform, uint32_t flags)
+{
+	Director* director = Director::getInstance();
+
+	CCASSERT(nullptr != director, "Director is null when seting matrix stack");
+	director->pushMatrix(MATRIX_STACK_TYPE::MATRIX_STACK_MODELVIEW);
+	director->loadMatrix(MATRIX_STACK_TYPE::MATRIX_STACK_MODELVIEW, transform);
+
+
+	GL::enableVertexAttribs(cocos2d::GL::VERTEX_ATTRIB_FLAG_POSITION);
+	world->DrawDebugData();
+	CHECK_GL_ERROR_DEBUG();
+
+	director->popMatrix(MATRIX_STACK_TYPE::MATRIX_STACK_MODELVIEW);
+	
+}
+
+void HelloWorld::onEnter()
+{
+	Scene::onEnter();
+
+	texture = Director::getInstance()->getTextureCache()->addImage("Globe_48px.png");
 
 	b2Body* body_droid;
 
 	b2BodyDef def_droid; //바디설정기본값 .type = static
+	
+	auto spr_droid = Sprite::create("Android_48px.png");
+	spr_droid->setPosition(200, 50);
+	this->addChild(spr_droid, 0, "spr_droid");
+
+	if (!debug) {
+		
+		def_droid.userData = spr_droid;
+	}
+	else {
+		spr_droid->setVisible(false);
+		def_droid.userData = nullptr;
+	}
+
 	def_droid.position.x = ((Sprite*)this->getChildByName("spr_droid"))->getPosition().x / PTM_RATIO;
 	def_droid.position.y = ((Sprite*)this->getChildByName("spr_droid"))->getPosition().y / PTM_RATIO;
-	def_droid.userData = spr_droid;
 
 	body_droid = world->CreateBody(&def_droid);
 
 	b2FixtureDef fixdef_droid;
 	b2PolygonShape polygon_droid;
-	polygon_droid.SetAsBox(31 / PTM_RATIO, 31 / PTM_RATIO);
+	polygon_droid.SetAsBox(20.0f / PTM_RATIO, 20.0f / PTM_RATIO);
 
 	fixdef_droid.shape = &polygon_droid;
 	fixdef_droid.density = 0.0f;
 	fixdef_droid.restitution = 0.7f;
 
 	body_droid->CreateFixture(&fixdef_droid);
-////////
-
-	this->schedule(schedule_selector(HelloWorld::tick));
-
-	return true;
-}
-
-void HelloWorld::onEnter()
-{
-	Scene::onEnter();
 
 	auto listener = EventListenerTouchOneByOne::create();
 	listener->setSwallowTouches(true);
@@ -118,22 +174,28 @@ bool HelloWorld::onTouchBegan(Touch * touch, Event * event)
 
 void HelloWorld::addNewSpriteAtPosition(Vec2 location)
 {
-	auto spr = Sprite::createWithTexture(texture);
-	spr->setPosition(location.x, location.y);
-	this->addChild(spr);
-	
 	b2Body* body_spr;
 	b2BodyDef body_spr_Def;
-	
+
+	//debug//
+	if (!debug) {
+		auto spr = Sprite::createWithTexture(texture);
+		spr->setPosition(location.x, location.y);
+		this->addChild(spr);
+		body_spr_Def.userData = spr;
+	}
+	else body_spr_Def.userData = nullptr;
+	////
+
 	body_spr_Def.type = b2_dynamicBody;
 	body_spr_Def.position.Set(location.x / PTM_RATIO, location.y / PTM_RATIO);
-	body_spr_Def.userData = spr;
+	
 
 	body_spr = world->CreateBody(&body_spr_Def);  //바디만들고 바디def설정완료
 
 	b2FixtureDef fix_spr_def;	 //픽스쳐설정객체 생성
 	b2CircleShape cir;
-	cir.m_radius = 0.65;
+	cir.m_radius = 0.65f;
 
 	//픽스쳐설정설정
 	fix_spr_def.shape = &cir;  
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -1,5 +1,7 @@
 #include "cocos2d.h"
 #include "Box2D\Box2D.h"
+#include <GLES-Render.h>
+
 #pragma execution_character_set("utf-8")
 
 USING_NS_CC;
@@ -11,6 +13,7 @@ class HelloWorld : public Scene {
 
 public:
 
+	bool debug = false;
 
 	static Scene* createScene();
 	virtual bool init();
@@ -18,9 +21,18 @@ public:
 	Texture2D* texture;
 	b2World* world;
 
+	//debugMode//
+	GLESDebugDraw* debugDraw;
+	CustomCommand custom_cmd;
+	
+	virtual void draw(Renderer* renderer, const Mat4& transform, uint32_t flags) override;
+	void onDraw(const Mat4& transform, uint32_t flags);
+	////
+
 	void onEnter();
 	void tick(float dt);
 
+	bool createBox2dWorld(bool debug);
 	bool onTouchBegan(Touch* touch, Event* event);
 	void addNewSpriteAtPosition(Vec2 location);
 
